d2a5e071b07638f2c806de2e497035f4695845aa
Je Vi Tan <57917899+JeviTan@users.noreply.github.com>
2019-11-24 23:44:48 -0600

    Input and submit button

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,11 +1,28 @@
-import React from 'react'
+import React from 'react';
+import {MdMoodBad} from "react-icons/md";
 
 const ExpenseForm = () => {
     return (
-        <div>
-            Hello from ExpenseForm.js
-        </div>
-    )
+        <form>
+            <div className="form-center">
+                <div className="form-group">
+                    <label htmlFor="charge">Category</label>
+                    <input type="text" className="form-control" id="charge" name="charge" placeholder="What Expense?" 
+                    />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="amount">Amount</label>
+                    <input type="text" className="form-control" id="amount" name="amount" placeholder="How Much?" 
+                    />
+                </div>
+            </div>
+            <button type="submit" className="btn">
+                submit
+                <MdMoodBad className="btn-icon" />
+            </button>
+
+        </form>
+    );
 }
 
 export default ExpenseForm
