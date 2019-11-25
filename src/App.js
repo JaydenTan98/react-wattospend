53c56cf2912023011f75e3df20be3e81c2e85954
Je Vi Tan <jevitan@Jes-MacBook-Pro-2.local>
2019-11-24 22:58:03 -0600

    Total Spending and background

--- src/App.js
+++ src/App.js
@@ -1,15 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import ExpenseList from './components/ExpenseList';
 import ExpenseForm from './components/ExpenseForm';
 import Alert from './components/Alert';
+import uuid from 'uuid/v4'
+
+const initialExpenses = [{ 
+  id: uuid(),
+  charge: "rent",
+  amount: 1200
+  },
+  {
+    id: uuid(),
+    charge: "car",
+    amount: 1200
+  },
+  {
+    id: uuid(),
+    charge: "mom",
+    amount: 300
+  }
+];
+
 
 function App() {
-  return <>
-    <Alert />
-    <ExpenseForm />
-    <ExpenseList />
-  </>;
+  const [expenses, setExpenses] = useState(initialExpenses);
+
+
+  return (
+    <>
+      <Alert />
+      <h1>Budget Calculator</h1>
+      <main className="App">
+        <ExpenseForm />
+        <ExpenseList expenses={expenses} />
+      </main>
+      <h1>
+        Total Spending: <span className="total">
+          $ {expenses.reduce((acc, curr)=>{
+            return (acc += curr.amount) ;
+          }, 0)}
+        </span>
+      </h1>
+    </>
+  );
 }
 
 export default App;
