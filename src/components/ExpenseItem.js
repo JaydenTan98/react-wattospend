d6667369bb55f01017a914238d1767aa2ec841ad
Je Vi Tan <57917899+JeviTan@users.noreply.github.com>
2019-11-24 23:33:41 -0600

    List of expenses and icons for delete and edit

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from 'react';
+import {MdEdit, MdDeleteForever} from 'react-icons/md';
 
-const ExpenseItem = () => {
+const ExpenseItem = ({expense}) => {
+    const {id, charge, amount} = expense 
     return (
-        <div>
-            Hello from ExpenseItem.js
-        </div>
-    )
+        <li className="item">
+            <div className="info">
+                <span className="expense">{charge}</span>
+                <span className="amount">${amount}</span>
+            </div>
+            <div>
+                <button className="edit-btn" aria-label="edit button">
+                    <MdEdit/>
+                </button>
+                <button className="clear-btn" aria-label="delete button">
+                    <MdDeleteForever/>
+                </button>
+            </div>
+        </li>
+    );
 }
 
-export default ExpenseItem
+export default ExpenseItem;
