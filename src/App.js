f5134e318fc5f078312ce01ff07812e9b7154be8
Je Vi Tan <57917899+JeviTan@users.noreply.github.com>
2019-11-25 20:20:27 -0600

    Submit expense function

--- src/App.js
+++ src/App.js
@@ -24,21 +24,53 @@ const initialExpenses = [{
 
 
 function App() {
+  //all of the expenses, add expenses
   const [expenses, setExpenses] = useState(initialExpenses);
+  //single expense
+  const [charge, setCharge] = useState("");
+  //single amount
+  const [amount, setAmount] = useState("");
 
+  //functions to handle
+  const handleCharge = e =>{
+    setCharge(e.target.value)
+  };
+  //functions to handle
+  const handleAmount = e =>{
+    setAmount(e.target.value)
+  };
+  
+  const handleSubmit = e =>{
+    e.preventDefault();
+    if(charge !== "" && amount > 0){  //Name of expense and amount conditions
+      const singleExpense = {id: uuid(), charge, amount};
+      setExpenses([...expenses, singleExpense]);
+      setCharge("");
+      setAmount("");
+    }
+    else{
+
+    }
+  };
 
   return (
     <>
       <Alert />
       <h1>Budget Calculator</h1>
       <main className="App">
-        <ExpenseForm />
+        <ExpenseForm 
+        charge={charge} 
+        amount={amount} 
+        handleAmount={handleAmount} 
+        handleCharge={handleCharge} 
+        handleSubmit={handleSubmit} /
+        >
         <ExpenseList expenses={expenses} />
       </main>
       <h1>
         Total Spending: <span className="total">
           $ {expenses.reduce((acc, curr)=>{
-            return (acc += curr.amount) ;
+            return (acc += parseInt(curr.amount)) ; //integer coersion
           }, 0)}
         </span>
       </h1>
--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import {MdMoodBad} from "react-icons/md";
 
-const ExpenseForm = () => {
+const ExpenseForm = ({charge,amount,handleCharge,handleAmount,handleSubmit}) => {
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <div className="form-center">
                 <div className="form-group">
                     <label htmlFor="charge">Category</label>
                     <input type="text" className="form-control" id="charge" name="charge" placeholder="What Expense?" 
+                        value={charge} onChange={handleCharge} 
                     />
                 </div>
                 <div className="form-group">
                     <label htmlFor="amount">Amount</label>
-                    <input type="text" className="form-control" id="amount" name="amount" placeholder="How Much?" 
+                    <input type="number" className="form-control" id="amount" name="amount" placeholder="How Much?" 
+                        value={amount} onChange={handleAmount}
                     />
                 </div>
             </div>
